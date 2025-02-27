0826c48dd31a88a2ceb1ea22499dc1510f1e3c22
Israel <israel.pe@outlook.es>
2025-02-27 16:47:10 -0500

    feat: added envs management

---
+++ src/config/envs.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+import * as process from 'node:process';
+
+interface EnvVars {
+    PORT: number;
+    NATS_SERVER: string;
+}
+
+const envsSchema = joi
+    .object({
+        PORT: joi.number().required(),
+        NATS_SERVER: joi.string().required(),
+    })
+    .unknown(true);
+
+const {error, value} = envsSchema.validate(process.env);
+
+if (error) throw new Error(`Config validation Error: ${error.message}`);
+
+const envVars: EnvVars = value;
+
+export const envs = {
+    port: envVars.PORT,
+    natsServer: envVars.NATS_SERVER,
+};

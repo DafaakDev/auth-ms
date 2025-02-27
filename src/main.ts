6796edf0895f005e3f426742f0f982e79d62c633
Israel <israel.pe@outlook.es>
2025-02-27 16:46:24 -0500

    feat: change to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {Logger, ValidationPipe} from "@nestjs/common";
+import {Transport} from "@nestjs/microservices";
+import {envs} from "./config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+    const logger = new Logger('AUTH-MS');
+    //todo
+    // hacerlo microservicio y conectarlo con el gateway
+    // configurar docker
+    // envs
+
+    const app = await NestFactory.createMicroservice(AppModule,
+        {
+            transport: Transport.NATS,
+            options: {
+                servers: [envs.natsServer],
+            },
+        }
+    );
+
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    await app.listen();
+    logger.log(`AUTH-MS running on port ${envs.port}`);
 }
+
 bootstrap();
